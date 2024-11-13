a768a669df89448c91f357905df18c6d2b9d1c17
vrbq <richard.sandeau.pro@gmail.com>
2024-11-13 15:14:02 +0100

    correct cities

--- modules/morse/remise_zero.sh
+++ modules/morse/remise_zero.sh
@@ -18,10 +18,10 @@ else
 
     # Supprimer tous les fichiers de couleur existants
     # Vérifie si chafa est installé
-    if dpkg -l | grep -q chafa; then
-        # Désinstaller la librairie chafa
-        sudo apt remove --purge -y chafa > /dev/null 2>&1
-    fi
+    # if dpkg -l | grep -q chafa; then
+    #     # Désinstaller la librairie chafa
+    #     sudo apt remove --purge -y chafa > /dev/null 2>&1
+    # fi
 
     # Supprimer les fichiers spécifiques et .encoded
     rm -f *.csv *.hdf *.obj *.svg .encoded requete.txt
